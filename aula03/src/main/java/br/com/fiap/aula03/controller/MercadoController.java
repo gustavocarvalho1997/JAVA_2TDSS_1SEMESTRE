2f07ea1408ff23def48debc71505fd9ad996a5e1
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-05 22:50:13 -0300

    inicio JPA e Driver Oracle

--- aula03/src/main/java/br/com/fiap/aula03/controller/MercadoController.java
+++ aula03/src/main/java/br/com/fiap/aula03/controller/MercadoController.java
@@ -3,6 +3,8 @@ package br.com.fiap.aula03.controller;
 import br.com.fiap.aula03.dto.CadastroMercadoDto;
 import br.com.fiap.aula03.model.CategoriaMercado;
 import br.com.fiap.aula03.model.Mercado;
+import br.com.fiap.aula03.repository.MercadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -10,6 +12,9 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 @RequestMapping("/mercados")
 public class MercadoController {
+    // Injeção de dependencia, o spring coloca o objeto no atributo
+    @Autowired
+    private MercadoRepository mercadoRepository;
     // GET
     @GetMapping("/consultar")
     public Mercado listar() {
@@ -18,8 +23,9 @@ public class MercadoController {
 
     // POST
     @PostMapping
-    public ResponseEntity<Mercado> cadastrar(@RequestBody CadastroMercadoDto dto, UriComponentsBuilder uriBuilder) {
-        var url = uriBuilder.path("/mercados/{id}").buildAndExpand(1).toUri();
-        return ResponseEntity.created(url).body(new Mercado(1, dto.nome(), dto.categoria()));
+    public ResponseEntity<Mercado> cadastrar(@RequestBody Mercado dto, UriComponentsBuilder uriBuilder) {
+        mercadoRepository.save(dto);
+        var url = uriBuilder.path("/mercados/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(url).body(dto);
     }//POST
 }//CLASS

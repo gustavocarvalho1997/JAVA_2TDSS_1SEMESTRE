cc5f174235a4051ded9569cc2e5da5b8812fe8a6
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-18 16:02:36 -0300

    Acrescentado o método para atualização das informações

--- aula03/src/main/java/br/com/fiap/aula03/model/Produto.java
+++ aula03/src/main/java/br/com/fiap/aula03/model/Produto.java
@@ -1,5 +1,6 @@
 package br.com.fiap.aula03.model;
 
+import br.com.fiap.aula03.dto.produto.AtualizacaoProdutoDTO;
 import br.com.fiap.aula03.dto.produto.CadastroProdutoDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -32,6 +33,19 @@ public class Produto {
         this.nome = produtoDto.nome();
         this.valor = produtoDto.valor();
         this.quantidade = produtoDto.quantidade();
+    }
+
+    // Atualiza as informações do produto
+    public void atualizarInformacoes(AtualizacaoProdutoDTO dto) {
+        if(dto.nome() != null) {
+            this.nome = dto.nome();
+        }
+        if(dto.valor() != null) {
+            this.valor = dto.valor();
+        }
+        if(dto.quantidade() != null) {
+            this.quantidade = dto.quantidade();
+        }
     }
 
 }

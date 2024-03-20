876c976c9a7a275db26cfdf9cda95ded08ec1397
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-20 02:52:40 -0300

    Corrigido tipo da data de cadastro

--- aula04/src/main/java/br/com/fiap/store/aula04/dto/DetalhesClienteDTO.java
+++ aula04/src/main/java/br/com/fiap/store/aula04/dto/DetalhesClienteDTO.java
@@ -5,9 +5,10 @@ import br.com.fiap.store.aula04.model.Cliente;
 import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-public record DetalhesClienteDTO(Long codigo, String nome, String cpf, LocalDate dataNascimento, LocalDate dataCadastro, CategoriaCliente categoria, Integer pontos) {
+public record DetalhesClienteDTO(Long codigo, String nome, String cpf, LocalDate dataNascimento, LocalDateTime dataCadastro, CategoriaCliente categoria, Integer pontos) {
     public DetalhesClienteDTO(Cliente cliente) {
-        this(cliente.getCodigo(), cliente.getNome(), cliente.getCpf(), cliente.getDataNascimento(), cliente.getDataCadastro().toLocalDate(), cliente.getCategoria(), cliente.getPontos());
+        this(cliente.getCodigo(), cliente.getNome(), cliente.getCpf(), cliente.getDataNascimento(), cliente.getDataCadastro(), cliente.getCategoria(), cliente.getPontos());
     }
 }// RECORD

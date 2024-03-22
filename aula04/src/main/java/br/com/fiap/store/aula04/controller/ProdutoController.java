c4e3ab451c19d1e720212019277aa8fa6616ae1a
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-22 14:14:59 -0300

    Iniciado ProdutoController

---
+++ aula04/src/main/java/br/com/fiap/store/aula04/controller/ProdutoController.java
@@ -0,0 +1,14 @@
+package br.com.fiap.store.aula04.controller;
+
+import br.com.fiap.store.aula04.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+}

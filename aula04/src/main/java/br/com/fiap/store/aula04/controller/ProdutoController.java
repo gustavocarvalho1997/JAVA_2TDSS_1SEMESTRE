5adada312aa599e2001b36c376a4cb6c96cc36ce
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-26 13:17:35 -0300

    implementado método para retornar a lista de produtos

--- aula04/src/main/java/br/com/fiap/store/aula04/controller/ProdutoController.java
+++ aula04/src/main/java/br/com/fiap/store/aula04/controller/ProdutoController.java
@@ -2,16 +2,17 @@ package br.com.fiap.store.aula04.controller;
 
 import br.com.fiap.store.aula04.dto.produto.CadastroProdutoDTO;
 import br.com.fiap.store.aula04.dto.produto.DetalhesProdutoDTO;
+import br.com.fiap.store.aula04.dto.produto.ListagemProdutoDTO;
 import br.com.fiap.store.aula04.model.Produto;
 import br.com.fiap.store.aula04.repository.ProdutoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -28,5 +29,12 @@ public class ProdutoController {
         produtoRepository.save(produto);
         var url = uri.path("/produtos/{id}").buildAndExpand(produto.getCodigo()).toUri();
         return ResponseEntity.created(url).body(new DetalhesProdutoDTO(produto));
+    }
+
+    // GET all
+    @GetMapping
+    public ResponseEntity<List<ListagemProdutoDTO>> listar(Pageable paginacao) {
+        var lista = produtoRepository.findAll(paginacao).stream().map(ListagemProdutoDTO::new).toList();
+        return ResponseEntity.ok(lista);
     }
 }

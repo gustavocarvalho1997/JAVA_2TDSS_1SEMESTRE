b779c6b350aa495afb4d88f5fb71ed041fca3c95
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-04-16 21:57:35 -0300

    Implementado DetalhesPostRepository

---
+++ exercicio/src/main/java/br/com/fiap/exercicio/repository/DetalhesPostRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.exercicio.repository;
+
+import br.com.fiap.exercicio.model.DetalhesPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalhesPostRepository extends JpaRepository<DetalhesPost, Long> {
+}// Interface

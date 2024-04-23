06e5156d407ae71e1f55bdd647d65ad3c7f65bde
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-04-23 12:22:23 -0300

    Criado o DTO responsavel por retornar os Detalhes

--- exercicio/src/main/java/br/com/fiap/exercicio/dto/DetalhesPostDTO.java
+++ exercicio/src/main/java/br/com/fiap/exercicio/dto/DetalhesPostDTO.java
@@ -2,8 +2,14 @@ package br.com.fiap.exercicio.dto;
 
 import br.com.fiap.exercicio.model.Post;
 
-public record DetalhesPostDTO(Long codigo, String titulo, String conteudo) {
+import java.time.LocalDateTime;
+
+public record DetalhesPostDTO(Long codigo, String titulo, String conteudo,
+                              String autor, LocalDateTime dataCriacao, LocalDateTime dataPublicacao) {
+
     public DetalhesPostDTO(Post post){
-        this(post.getCodigo(), post.getTitulo(), post.getConteudo());
+        this(post.getCodigo(), post.getTitulo(), post.getConteudo(), post.getDetalhesPost().getNomeAutor(),
+                post.getDetalhesPost().getDataCriacao(), post.getDetalhesPost().getDataPublicacao());
     }
-}// RECORD
+
+}

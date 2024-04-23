dcebc3a742ad794539044f20a0279974c650db6b
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-04-23 12:22:59 -0300

    Criado o DTO responsavel pelo cadastro

--- exercicio/src/main/java/br/com/fiap/exercicio/dto/CadastroPostDTO.java
+++ exercicio/src/main/java/br/com/fiap/exercicio/dto/CadastroPostDTO.java
@@ -1,14 +1,19 @@
 package br.com.fiap.exercicio.dto;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.time.LocalDateTime;
+
 public record CadastroPostDTO(
-        @NotBlank
-        @Size(max = 50, message = "O tamanho máximo do titulo deve ser 50 caracteres")
+        @NotBlank @Size(max = 50)
         String titulo,
-        @NotBlank
-        @Size(max = 500, message = "O conteudo deve conter no máximo 500 caracteres")
-        String conteudo
-) {
-}// RECORD
+        @NotBlank @Size(max = 500)
+        String conteudo,
+        @NotBlank @Size(max = 50)
+        String nomeAutor,
+        @Future @NotNull
+        LocalDateTime dataPublicacao) {
+}
